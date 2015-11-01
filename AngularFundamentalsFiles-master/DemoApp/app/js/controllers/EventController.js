edd3facd6cd50539ee8090cc37fae6a294ca72e4
nvwoods <nvincentwoods@gmail.com>
2015-11-01 20:34:39 +0000

    1 Nov eod commit
    Committing changes for the day.  Worked through Pluralsight tutorials,
    finishing the DEMO Handling Events session.  Events details page now has
    up/downvote buttons that respond to mouse click events.

---
+++ AngularFundamentalsFiles-master/DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,51 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventsController($scope) {
+        $scope.event = {
+            name: 'Angular Boot Camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            location: {
+                address: 'Google Headquarters',
+                city: 'Mountain View',
+                province: 'CA'
+            },
+            imageUrl: '/img/angularjs-logo.png',
+            sessions: [
+                {
+                    name: 'Directives Masterclass',
+                    creatorName: 'Bob Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session you will learn the ins and outs of directives!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for Fun and Profit',
+                    creatorName: 'Joe Bloggs',
+                    duration: '2 hrs',
+                    level: 'Intermediate',
+                    abstract: 'Learn about scopes and stuff.  It\'s fun and profitable!',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well Behaved Controllers',
+                    creatorName: 'Stanislaw Pietorov',
+                    duration: '1.5 hrs',
+                    level: 'Beginner',
+                    abstract: 'Discipline your controllers to get the best out of them.',
+                    upVoteCount: 0
+                }
+            ]
+        }
+
+        $scope.upVoteSession = function(session) {
+            session.upVoteCount++;
+        };
+
+        $scope.downVoteSession = function(session) {
+            session.upVoteCount--;
+        };
+    }
+);
